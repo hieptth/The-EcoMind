1191adbcf2ca8dd577b33f58428162f4a3a5c543
hieptth <hieptth71@gmail.com>
2024-06-02 21:04:41 +0700

    update

--- src/app/dashboard/MessagesContent.tsx
+++ src/app/dashboard/MessagesContent.tsx
@@ -1,8 +1,16 @@
 // ChartsContent.tsx
 import React from "react";
+import "./message.css";
+import { Card } from "antd";
 
 const MessagesContent = () => {
-  return <div>Message content goes here</div>;
+  return (
+    <div className="page-container">
+      <Card title="Messages" className="chart-card">
+        <div className="main-content">Hello</div>
+      </Card>
+    </div>
+  );
 };
 
 export default MessagesContent;
